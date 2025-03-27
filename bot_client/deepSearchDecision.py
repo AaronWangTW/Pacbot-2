d70ea6a2af08f36739f6c2f3bd770a6cf5c31047
AaronWangTW <honglin2023@gmail.com>
2025-03-26 23:18:23 -0500

    old heuristics eval draft

--- bot_client/deepSearchDecision.py
+++ bot_client/deepSearchDecision.py
@@ -17,6 +17,14 @@ D_MESSAGES = [b"w", b"a", b"s", b"d", b"."]
 TICK_ESTIMATE_BY_LEVEL = [12, int(12 * 1.5), 12 * 2, int(12 * 2.5), 12 * 3]
 FOOD_POSITIONS = []
 
+PELLET_WEIGHT = 0.65
+GHOST_WEIGHT = 0.35
+FRIGHTENED_GHOST_WEIGHT = 2 * GHOST_WEIGHT
+GHOST_CUTOFF = 10
+GHOST_FRIGHTENED_CUTOFF = 13
+CHERRY_WEIGHT = 2 * PELLET_WEIGHT
+REVERSE_WEIGHT = 15
+
 gs = GameState()
 
 for col in range(28):
@@ -24,6 +32,32 @@ for col in range(28):
         if gs.pelletAt(row, col):
             FOOD_POSITIONS.append((col, row))
 
+def bfs(grid, start, target, max_dist=float("inf")):
+    visited = []
+    queue = [(start, [])]
+
+    while len(queue) > 0:
+        nxt = queue.pop(0)
+        visited.append(nxt[0])
+        new_path = copy.deepcopy(nxt[1])
+        new_path.append(nxt[0])
+        loc = nxt[0]
+        if type(target) is tuple:
+            if target == loc:
+                return new_path
+        elif grid[loc[0]][loc[1]] in target:
+            return new_path
+
+        if grid[loc[0] + 1][loc[1]] in [o, e, O] and (loc[0] + 1, loc[1]) not in visited and len(new_path) <= max_dist:
+            queue.append(((loc[0] + 1, loc[1]),new_path))
+        if grid[loc[0] - 1][loc[1]] in [o, e, O] and (loc[0] - 1, loc[1]) not in visited and len(new_path) <= max_dist:
+            queue.append(((loc[0] - 1, loc[1]),new_path))
+        if grid[loc[0]][loc[1] + 1] in [o, e, O] and (loc[0], loc[1] + 1) not in visited and len(new_path) <= max_dist:
+            queue.append(((loc[0], loc[1] + 1),new_path))
+        if grid[loc[0]][loc[1] - 1] in [o, e, O] and (loc[0], loc[1] - 1) not in visited and len(new_path) <= max_dist:
+            queue.append(((loc[0], loc[1] - 1),new_path))
+
+    return None
 
 class DeepDecisionModule:
     def __init__(self, state: GameState) -> None:
@@ -130,8 +164,61 @@ class DeepDecisionModule:
             + ghost_reward
             + fruit_bonus
             + ghost_penalty
-            - 1000 * (3 - game_state.currLives)
         )
+
+    def _find_distance_of_closest_pellet(self, target_loc):
+        return len(bfs(self.grid, target_loc, [o])) - 1
+
+    def _find_distance_of_closest_powerup(self, target_loc):
+        return len(bfs(self.grid, target_loc, [O])) - 1
+
+    def  _find_paths_to_closest_ghosts_state(self, state:GameState):
+        ghosts = state.ghosts
+        state_paths = [(ghost.frightSteps > 2, bfs(self.grid, (state.pacmanLoc.col,30-state.pacmanLoc.row), (ghost.location.col, 30 - ghost.location.row), GHOST_CUTOFF)) for ghost in ghosts]
+        return [sp for sp in state_paths if sp[1] is not None]
+
+    def _find_distance_to_cherry(self, target_loc):
+        return len(bfs(self.grid, target_loc, (self.state.fruitLoc.col, 30- self.state.fruitLoc.row))) - 1
+
+    def oldEvaluationFunction(self, game_state: GameState):
+        pacman_pos = np.array([game_state.pacmanLoc.row, game_state.pacmanLoc.col])
+        pacman_direction = game_state.pacmanLoc.getDirection()
+        score = game_state.currScore
+        pellet_arr = game_state.pelletArr
+            
+        if game_state.wallAt(pacman_pos[0],pacman_pos[1]):
+            return float('-inf')
+        
+        # Calculate pellet distance heuristic
+        if game_state.numPellets() - game_state.numPowerups == 0:  # No more power-ups
+            dist_to_pellet = self._find_distance_of_closest_powerup(pacman_pos)
+        else:
+            dist_to_pellet = self._find_distance_of_closest_pellet(pacman_pos)
+        
+        # Calculate ghost proximity heuristic (ghosts are bad, but frightened ghosts are good)
+        paths_to_ghosts = self._find_paths_to_closest_ghosts_state(game_state)
+        ghosts = []
+        ghost_heuristic = 0
+        for state, path in paths_to_ghosts:
+            dist = len(path) - 1
+            ghosts.append((state, dist))
+            if dist < GHOST_CUTOFF:
+                if state == False:  # Normal ghost (bad)
+                    ghost_heuristic += pow((GHOST_CUTOFF - dist), 2) * GHOST_WEIGHT
+                else:  # Frightened ghost (good)
+                    ghost_heuristic += pow((GHOST_CUTOFF - dist), 2) * -1 * FRIGHTENED_GHOST_WEIGHT
+        
+        # Pellet heuristic (closer to pellet = better)
+        pellet_heuristic = dist_to_pellet * PELLET_WEIGHT
+        
+        # Fruit heuristic (if there's a fruit, go towards it)
+        fruit_bonus = self._find_distance_to_cherry(pacman_pos) * CHERRY_WEIGHT if game_state.fruitLoc.col != 32 else 0
+        
+        # Combine all heuristics for this target
+        total_heuristic = pellet_heuristic + ghost_heuristic + fruit_bonus
+        
+        # Return the evaluated score for the best target (this is where we return the heuristic score)
+        return score + total_heuristic
 
     def worker(self):
         while True:
@@ -148,6 +235,7 @@ class DeepDecisionModule:
     def deepSearch(self, branch, depth, state: GameState):
         if state.currLives == 0 or depth >= self.depth or state.numPellets() == 0:
             return self.evaluationFunction(state)
+            # return self.oldEvaluationFunction(state)
 
         best_score = float("-inf")
 
@@ -159,17 +247,21 @@ class DeepDecisionModule:
             target = (p_loc.col + dx, p_loc.row + dy)
             if not state.wallAt(target[1], target[0]):
                 sim_state = fast_copy_game_state(state)
-                sim_state.simulateAction(
+                safe = sim_state.simulateAction(
                     TICK_ESTIMATE_BY_LEVEL[state.currLevel - 1], directions[i]
                 )
-                eval_score = self.evaluationFunction(sim_state)
+                if safe:
+                    eval_score = self.evaluationFunction(sim_state)
+                else:
+                    eval_score = float("-inf")
+                #eval_score = self.oldEvaluationFunction(sim_state)
                 moves.append((eval_score, sim_state))
 
         moves.sort(reverse=True, key=lambda x: x[0])  # Prioritize best moves
 
         for eval_score, sim_state in moves:
             best_score = max(best_score, self.deepSearch(branch, depth + 1, sim_state))
-
+        #print(best_score)
         return best_score
 
     def tick(self):
@@ -210,6 +302,7 @@ class DeepDecisionModule:
         if self.results:
             best_branch = max(self.results, key=self.results.get)
             self._send_command_message_to_target(directions[best_branch])
+            print(directions[best_branch])
 
     async def decisionLoop(self) -> None:
         while self.state.isConnected():
